9ec4798d1f26d612ee481f5da814aca96ee04774
hmhm <s-hasegawa@infocity.co.jp>
2016-03-30 13:02:05 +0900

    Initial Commit

---
+++ HomeScreenQuickActions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomeScreenQuickActions
+//
+//  Created by 長谷川瞬哉 on 2016/03/30.
+//  Copyright © 2016年 長谷川瞬哉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
